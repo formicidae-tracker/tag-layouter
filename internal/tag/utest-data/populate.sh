6917ea194fd1fe0ef0df2769d5ae3273fb194030
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2023-08-18 11:21:16 +0200

    Adds unittest data

---
+++ internal/tag/utest-data/populate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+wget https://github.com/AprilRobotics/apriltag-imgs/blob/master/tag16h5/tag16_05_00000.png?raw=true -O tag16h5_00000.png
+wget https://github.com/AprilRobotics/apriltag-imgs/blob/master/tag25h9/tag25_09_00000.png?raw=true -O tag25h9_00000.png
+wget https://github.com/AprilRobotics/apriltag-imgs/blob/master/tag36h11/tag36_11_00000.png?raw=true -O tag36h11_00000.png
+wget https://github.com/AprilRobotics/apriltag-imgs/blob/master/tagCircle21h7/tag21_07_00000.png?raw=true -O tagCircle21h7_00000.png
+wget https://github.com/AprilRobotics/apriltag-imgs/blob/master/tagCircle49h12/tag49_12_00000.png?raw=true -O tagCircle49h12_00000.png
+wget https://github.com/AprilRobotics/apriltag-imgs/blob/master/tagCustom48h12/tag48_12_00000.png?raw=true -O tagCustom48h12_00000.png
+wget https://github.com/AprilRobotics/apriltag-imgs/blob/master/tagStandard41h12/tag41_12_00000.png?raw=true -O tagStandard41h12_00000.png
+wget https://github.com/AprilRobotics/apriltag-imgs/blob/master/tagStandard52h13/tag52_13_00000.png?raw=true -O tagStandard52h13_00000.png
